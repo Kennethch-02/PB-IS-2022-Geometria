9ac4560755f495526e692d60ea515a42e348c1ca
Kenneth C <kennethalonsoc@gmail.com>
2022-03-07 19:50:40 -0600

    Actualizacion
    Se agregan los elementos, las coliciones, funcion de eliminar y agregar

--- Class.py
+++ Class.py
@@ -1,9 +1,12 @@
+from cmath import cos, sin
+import math
 import pygame
 
 class Triangulo(pygame.sprite.Sprite):
     def __init__(self, color, lado, posx, posy):
         pygame.sprite.Sprite.__init__(self)
         #Elementos que le pertenecen a la clase
+        self.type = "Triangulo"
         self.color = color
         self.height = ((3)**(1/2)*lado)/2
         self.weight = lado
@@ -20,12 +23,80 @@ class Triangulo(pygame.sprite.Sprite):
         self.third_point = (self.weight,self.height)
         self.position = [self.firt_point,self.second_point,self.third_point]
 
-        pygame.draw.polygon(self.image,self.color,self.position, 5)
+        pygame.draw.polygon(self.image,self.color,self.position, 0)
         self.mask = pygame.mask.from_surface(self.image)
 
+    def move(self, posx, posy):
+        self.rect.centerx = posx
+        self.rect.centery = posy
+
+class Pentagono(pygame.sprite.Sprite):
+    def __init__(self, color, lado, posx, posy):
+        pygame.sprite.Sprite.__init__(self)
+        #Elementos que le pertenecen a la clase
+        self.type = "Pentagono"
+        self.color = color
+        self.x = math.cos((72*math.pi)/180)*lado
+        self.x1 = math.sin((72*math.pi)/180)*lado
+        self.height = math.sin((54* math.pi)/180)*lado
+        self.weight = 2*(math.cos((72*math.pi)/180)*lado) + lado
+        
+        #Superficie
+        self.image = pygame.Surface([self.weight,self.weight],pygame.SRCALPHA,32)
+        #Elementos 
+        self.rect = self.image.get_rect()
+        self.rect.x = posx
+        self.rect.y = posy
+        #Dibuja un pentagono
+        self.first_point = (self.weight/2, 0)
+        self.second_point = (0,self.height)
+        self.third_point = (self.x,self.height+self.x1)
+        self.four_point = (self.x + lado,self.height+self.x1)
+        self.five_point = (self.weight,self.height)
+
+        self.position = [self.first_point,self.second_point,self.third_point,self.four_point,self.five_point]
+        pygame.draw.polygon(self.image,self.color,self.position, 0)
+        self.mask = pygame.mask.from_surface(self.image)
+    def move(self, posx, posy):
+        self.rect.centerx = posx
+        self.rect.centery = posy
+class Hexagono(pygame.sprite.Sprite):
+    def __init__(self, color, lado, posx, posy):
+        pygame.sprite.Sprite.__init__(self)
+        #Elementos que le pertenecen a la clase
+        self.type = "Hexagono"
+        self.color = color
+        self.x = math.sin((60*math.pi)/180)*lado
+        self.x1 = math.cos((60*math.pi)/180)*lado
+        self.height = self.x*2
+        self.weight = 2*self.x1 + lado
+        
+        #Superficie
+        self.image = pygame.Surface([self.weight,self.height],pygame.SRCALPHA,32)
+        #Elementos 
+        self.rect = self.image.get_rect()
+        self.rect.x = posx
+        self.rect.y = posy
+        #Dibuja un hexagono
+        self.first_point = (self.x1, 0)
+        self.second_point = (0,self.x)
+        self.third_point = (self.x1,self.height)
+        self.four_point = (self.x1+lado,self.height)
+        self.five_point = (self.weight,self.x)
+        self.six_point = (self.x1+lado,0)
+
+        self.position = [self.first_point,self.second_point,self.third_point,self.four_point,self.five_point,self.six_point]
+        print(self.position)
+        print(self.weight)
+        pygame.draw.polygon(self.image,self.color,self.position, 0)
+        self.mask = pygame.mask.from_surface(self.image)
+    def move(self, posx, posy):
+        self.rect.centerx = posx
+        self.rect.centery = posy
 class Cuadrado(pygame.sprite.Sprite):
     def __init__(self, color, lado, posx, posy):
         pygame.sprite.Sprite.__init__(self)
+        self.type = "Cuadrado"
         self.color = color
         self.height = lado
         self.weight = lado
@@ -37,4 +108,31 @@ class Cuadrado(pygame.sprite.Sprite):
         self.rect.y = posy
         #Dibujamos
         pygame.draw.rect(self.image,self.color,pygame.Rect(0,0,self.weight,self.height))
+    def move(self, posx, posy):
+        self.rect.centerx = posx
+        self.rect.centery = posy
+class Circulo(pygame.sprite.Sprite):
+    def __init__(self, color, radio, posx, posy):
+        pygame.sprite.Sprite.__init__(self)
+        self.type = "Circulo"
+        self.color = color
+        self.weight = 2*radio
+        self.height = 2*radio
+        #Superficie
+        self.image = pygame.Surface([self.weight, self.height], pygame.SRCALPHA, 32 )
+        #Recta
+        self.rect = self.image.get_rect()
+        self.rect.x = posx
+        self.rect.y = posy
+        #Dibujamos
+        pygame.draw.circle(self.image,self.color,(radio,radio),radio,0)
+    def move(self, posx, posy):
+        self.rect.centerx = posx
+        self.rect.centery = posy
 
+class Cursor(pygame.Rect):
+    def __init__(self):
+        pygame.Rect.__init__(self,0,0,1,1)
+        self.left,self.top = pygame.mouse.get_pos()
+    def update(self):
+        self.left,self.top = pygame.mouse.get_pos()
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 
+from asyncio.windows_events import NULL
+from msilib import type_binary
 from Class import *
 
 def MainWindow():
     pygame.init()
+    #Reloj
+    clock = pygame.time.Clock()
     #Grupo de Sprites
     sprite_list = pygame.sprite.Group()
+    sprite_list_menu = pygame.sprite.Group()
+
     pygame.display.set_caption("Proyecto Basico Geometria")
     #Dibujar la ventana
     screen = pygame.display.set_mode([1280,720])
+    #Crear un menu
+    
+    sprite_list_menu.add(Triangulo((0,0,255),100,50,600))
+    sprite_list_menu.add(Cuadrado((0,0,255),100,200,600))
+    sprite_list_menu.add(Pentagono((0,0,25),65,350,600))
+    sprite_list_menu.add(Hexagono((0,0,25),65,500,600))
+    sprite_list_menu.add(Circulo((0,0,25),55,670,600))
     #Add sprites
-    sprite_list.add(Triangulo((0,0,255),150,0,0))
-    sprite_list.add(Cuadrado((0,0,255),150,250,300))
+    Mouse = Cursor()
+    sprite_colide = NULL
+    news_sprites = 0
+    # Boolean para cuando el boton derecho esta precionado
+    mouseI = False
+    mouseD = False
     #Ciclo que corre la ventana principal
     run = True
     while run:
@@ -18,16 +35,63 @@ def MainWindow():
         for eventos in pygame.event.get():
             if eventos.type == pygame.QUIT:
                 run = False
+            if eventos.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_pressed()[0] == True:
+                    mouseI = True
+                elif pygame.mouse.get_pressed()[1] == True:
+                    pass
+                elif pygame.mouse.get_pressed()[2] == True:
+                    mouseD = True
+            if eventos.type == pygame.MOUSEBUTTONUP:
+                mouseI = False
+                mouseD = False
+                sprite_colide = NULL
+                news_sprites = 0
+
+        #Colisionns de los sprites
+        for sprites in sprite_list:
+            if (Mouse.colliderect(sprites.rect) and 
+            mouseI and sprite_colide==NULL):
+                sprite_colide = sprites
+
+            if (Mouse.colliderect(sprites.rect) and mouseD):
+                sprites.kill()
+
         
+        #Colisiones de los sprites del Menu
+        for sprites in sprite_list_menu:
+            if (Mouse.colliderect(sprites.rect) and mouseI and news_sprites == 0):
+                if (sprites.type  == "Triangulo"):
+                    sprite_list.add(Triangulo((255,255,0),50,Mouse.x,Mouse.y))
+                    news_sprites = 1
+                if (sprites.type  == "Cuadrado"):
+                    sprite_list.add(Cuadrado((255,255,0),50,Mouse.x,Mouse.y))
+                    news_sprites = 1
+                if (sprites.type  == "Circulo"):
+                    sprite_list.add(Circulo((255,255,0),50,Mouse.x,Mouse.y))
+                    news_sprites = 1
+                if (sprites.type  == "Pentagono"):
+                    sprite_list.add(Pentagono((255,255,0),50,Mouse.x,Mouse.y))
+                    news_sprites = 1
+                if (sprites.type  == "Hexagono"):
+                    sprite_list.add(Hexagono((255,255,0),50,Mouse.x,Mouse.y))
+                    news_sprites = 1
+
+        if mouseI and sprite_colide != NULL:
+            sprite_colide.move(Mouse.x,Mouse.y)
+
         #Agregarle un colo de fondo
         screen.fill((12,183,242))
+        pygame.draw.rect(screen,(255,89,87),pygame.Rect(0,575,1280,145))
         #Dibujar sprites
         sprite_list.draw(screen)
+        sprite_list_menu.draw(screen)
         #Actualizar la ventana
+        Mouse.update()
         sprite_list.update()
-        
+        sprite_list_menu.update()
         pygame.display.update()
-
+        clock.tick(60)
     pygame.quit()
     
 if __name__ == '__main__':
